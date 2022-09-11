8ef180b7770b34b26f16720987baaa0e93cf33cd
Dawid Zgierski <dzgierskigit@gmail.com>
2022-09-11 19:07:48 +0200

    product class added

--- src/js/dist/script.dev.js
+++ src/js/dist/script.dev.js
@@ -1,20 +1,24 @@
-/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+"use strict";
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/* global Handlebars, utils, dataSource */
+// eslint-disable-line no-unused-vars
 {
   'use strict';
 
-  const select = {
+  var select = {
     templateOf: {
-      menuProduct: '#template-menu-product',
+      menuProduct: '#template-menu-product'
     },
     containerOf: {
       menu: '#product-list',
-      cart: '#cart',
+      cart: '#cart'
     },
     all: {
       menuProducts: '#product-list > .product',
       menuProductsActive: '#product-list > .product.active',
-      formInputs: 'input, select',
+      formInputs: 'input, select'
     },
     menuProduct: {
       clickable: '.product__header',
@@ -22,59 +26,55 @@
       priceElem: '.product__total-price .price',
       imageWrapper: '.product__images',
       amountWidget: '.widget-amount',
-      cartButton: '[href="#add-to-cart"]',
+      cartButton: '[href="#add-to-cart"]'
     },
     widgets: {
       amount: {
         input: 'input[name="amount"]',
         linkDecrease: 'a[href="#less"]',
-        linkIncrease: 'a[href="#more"]',
-      },
-    },
+        linkIncrease: 'a[href="#more"]'
+      }
+    }
   };
-
-  const classNames = {
+  var classNames = {
     menuProduct: {
       wrapperActive: 'active',
-      imageVisible: 'active',
-    },
+      imageVisible: 'active'
+    }
   };
-
-  const settings = {
+  var settings = {
     amountWidget: {
       defaultValue: 1,
       defaultMin: 1,
-      defaultMax: 9,
+      defaultMax: 9
     }
   };
-
-  const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  var templates = {
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML)
   };
 
-  class Product{
-    constructor(){
-      const thisProduct = this;
+  var Product = function Product() {
+    _classCallCheck(this, Product);
 
-      console.log('new Product:', thisProduct);
-    }
-  }
+    var thisProduct = this;
+    console.log('new Product:', thisProduct);
+  };
 
-  const app = {
-    initMenu: function() {
-      const testProduct = new Product();
+  var app = {
+    initMenu: function initMenu() {
+      var testProduct = new Product();
       console.log('testProduct:', testProduct);
     },
-    init: function(){
-      const thisApp = this;
+    init: function init() {
+      var thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
       thisApp.initMenu();
-    },
+    }
   };
-
   app.init();
 }
+//# sourceMappingURL=script.dev.js.map
