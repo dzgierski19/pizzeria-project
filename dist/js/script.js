8e60066f63d0688851e23c5782c9e43965647185
Dawid Zgierski <dzgierskigit@gmail.com>
2022-09-15 01:21:43 +0200

    images added

--- dist/js/script.js
+++ dist/js/script.js
@@ -77,37 +77,27 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
-      console.log(thisProduct.imageWrapper);
     }
 
     initAccordion() {
       const thisProduct = this;
-      /* find the clickable trigger (the element that should react to clicking) */
-      // const accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      /* START: click event listener to trigger */
+      /* START: add event listener to clickable trigger on event click */
       thisProduct.accordionTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
         event.preventDefault();
         /* toggle active class on element of thisProduct */
-        thisProduct.element.classList.toggle('active');
-        /* find all active products */
-        const allActiveProducts = document.querySelectorAll('.product.active');
-        /* START LOOP: for each active product */
-        for (let activeProduct of allActiveProducts) {
-          /* START: if the active product isn't the element of thisProduct */
-          if( activeProduct != thisProduct.element) {
-            /* remove class active for the active product */
-            activeProduct.classList.remove('active');
-          /* END: if the active product isn't the element of thisProduct */
-          }
-        /* END LOOP: for each active product */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+        // console.log(activeProduct);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct != null && activeProduct != thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
-      /* END: click event listener to trigger */
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
     initOrderForm() {
       const thisProduct = this;
-
       thisProduct.form.addEventListener('submit',function(event){
         event.preventDefault();
         thisProduct.processOrder();
@@ -137,10 +127,11 @@
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          const selected = formData[paramId].includes(optionId);
-          // console.log(optionId, option);
+          // find pictude associated with addon
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && selected) {
+          if(optionSelected) {
             // check if the option is not default
             if(!option.default) {
               // add option price to price variable
@@ -156,14 +147,20 @@
             price -= option.price;
             /* END ELSE IF: if option is not selected and option is default */
           }
-        /* END LOOP: for each optionId in param.options */
+          if(optionImage) {
+            if(optionSelected) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
         }
-      /* END LOOP: for each paramId in thisProduct.data.params */
+        /* set the contents of thisProduct.priceElem to be the value of variable price */
+        thisProduct.priceElem.innerHTML = price;
       }
-      /* set the contents of thisProduct.priceElem to be the value of variable price */
-      thisProduct.priceElem.innerHTML = price;
     }
   }
+
   const app = {
     initMenu: function () {
 
